c2b18cb8f52f2ae84a9a876d99b9470993691c6d
YCDM03 <ycdm03@gmail.com>
2024-05-21 12:45:13 +0900

    refactor: FilterButtons.jsx onClick event functions logic

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,37 +1,35 @@
 // TODO: FilterButtons 컴포넌트를 작성하세요. 필터링 옵션을 선택하는 버튼들을 포함해야 합니다.
 function FilterButtons({ setFilteredStudents, initialStudents }) {
-  const filterByAge = (minAge) => {
-    const filteredByAge = initialStudents.filter((student) => {
-      return student.age >= minAge;
-    });
-    setFilteredStudents(filteredByAge);
+  const baseAge = 18;
+  const baseGrade = "A";
+  const filterByAge = () => {
+    const ageFilter = (minAge) => {
+      const filteredByAge = initialStudents.filter((student) => {
+        return student.age >= minAge;
+      });
+      setFilteredStudents(filteredByAge);
+    };
+    ageFilter(baseAge);
   };
-  const filterByGrade = (grade) => {
-    const filteredByGrade = initialStudents.filter((student) => {
-      return student.grade === grade;
-    });
-    setFilteredStudents(filteredByGrade);
+
+  const filterByGrade = () => {
+    const gradeFillter = (grade) => {
+      const filteredByGrade = initialStudents.filter((student) => {
+        return student.grade === grade;
+      });
+      setFilteredStudents(filteredByGrade);
+    };
+    gradeFillter(baseGrade);
   };
+
   const resetFilter = () => {
     setFilteredStudents(initialStudents);
   };
 
   return (
     <div>
-      <button
-        onClick={() => {
-          filterByAge(18);
-        }}
-      >
-        18세 이상
-      </button>
-      <button
-        onClick={() => {
-          filterByGrade("A");
-        }}
-      >
-        A등급
-      </button>
+      <button onClick={filterByAge}>18세 이상</button>
+      <button onClick={filterByGrade}>A등급</button>
       <button onClick={resetFilter}>필터 초기화</button>
     </div>
   );
